f77f308cd139108fece633ba04efe85264037f6e
rm95266 <rm95266@estudante.fieb.edu.br>
2025-04-04 08:32:28 -0300

    kkk

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import { useState } from "react"
-
+ 
 function App(){
-
-  const [nome,setNome] = useState('ronaldo')
-
+ 
+  const [nome, setNome] = useState('Ronaldo')
+ 
+  const Formulario = () => {
+ 
+    return (
+      <>
+        <h3> Formulário </h3>
+       
+      <input
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type="text" />
+ 
+      <button
+        className="botao"      
+        onClick={()=>{
+          alert('O nome digita foi ' + nome)
+        }
+        }
+      >
+        CLIQUE AQUI
+      </button>
+ 
+      </>
+    )
+  }
+ 
   return (
     <div>
-      <h3>Pizarria 2D</h3>
-
-      <input
-       className="nome"
-       onChange={(e)=>{setNome(e.target.value)}}
-       type="text"/>
-
-       <button
-        className="botao"
-        onClick={()=>{alert(nome)}}
-        >
-          CLIQUE AQUI
-        </button>
-
-      </div>
+      <h3>Pizzaria 2D</h3>
+     
+      <Formulario />
+      
+ 
+    </div>
   )
 }
-
+ 
 export default App
